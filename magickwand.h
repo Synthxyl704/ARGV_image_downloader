78ec86c29b0f439d9ebee8b3317b1fc813a9deeb
IsoAris -RESURRECTION- <159614599+Synthxyl704@users.noreply.github.com>
2025-06-30 03:11:55 +0530

    added GSC function prototype

--- magickwand.h
+++ magickwand.h
@@ -1,6 +1,9 @@
-#ifndef MAGICKWAND_H
-#define MAGICKWAND_H
+#ifndef MAGICKWAND_UTILS_H
+#define MAGICKWAND_UTILS_H
+
+#include <MagickWand/MagickWand.h>
 
 int resizeImage(const char *inputFile, const char *outputFile, const char *resolution);
+int grayscaleImage(const char *inputPath, const char *outputPath);
 
 #endif
