8e2cb1d17f3f01a79fd5b080abb6d6d51d2d9f0b
[ITLX] | IsoAris7 <159614599+Synthxyl704@users.noreply.github.com>
2025-07-19 21:23:29 +0530

    added rotational function prototype

--- magickwand.h
+++ magickwand.h
@@ -3,7 +3,10 @@
 
 #include <MagickWand/MagickWand.h>
 
+// resizing + grayscaling | MACX
 int resizeImage(const char *inputFile, const char *outputFile, const char *resolution);
 int grayscaleImage(const char *inputPath, const char *outputPath);
+
+int rotateImage(const char *inputFile, const char *outputFile, const char *rotationCommand);
 
 #endif
